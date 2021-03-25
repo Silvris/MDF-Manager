2e04821399b6f29de77abcd7d7580aaff7325bd5
Silvris <58583688+Silvris@users.noreply.github.com>
2021-03-24 23:49:11 -0500

    begin on actual C# work

---
+++ MDF-Manager/Classes/TextureBinding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDF_Manager.Classes
+{
+    class TextureBinding
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+
+        public TextureBinding()//this should never be used, but needed for JSON iirc
+        {
+            name = "BaseMetalMap";
+            path = "systems/rendering/nullblack.tex";
+        }
+        public TextureBinding(string pName, string pPath)
+        {
+            name = pName;
+            path = pPath;
+        }
+
+    }
+}
---
+++ MDF-Manager/Classes/VariableProperty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDF_Manager.Classes
+{
+    public class Float
+    {
+        public float data { get; set; }
+        public Float(float fData)
+        {
+            data = fData;
+        }
+    }
+    public class Float4
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public float w { get; set; }
+        public Float4(float fX, float fY, float fZ, float fW)
+        {
+            x = fX;
+            y = fY;
+            z = fZ;
+            w = fW;
+        }
+    }
+    interface IVariableProp
+    {
+        public string name { get; set; }
+        public int size { get; set; }
+        public string type { get; set; }
+        public object defaultValue { get; set; }
+    }
+    public class FloatProperty : IVariableProp
+    {
+        private string _Name;
+        private int _Size;//this isn't really important, but its good to keep for JSON purposes
+        private string _Type;
+        private Float _Default;
+        public string name { get => _Name; set => _Name = value; }
+        public int size { get => _Size; set => _Size = value; }
+        public string type { get => _Type; set => _Type = value; }
+        public object defaultValue { get => _Default; set => _Default = (Float)value; }
+    }
+    public class Float4Property : IVariableProp
+    {
+        private string _Name;
+        private int _Size;//this isn't really important, but its good to keep for JSON purposes
+        private string _Type;
+        private Float4 _Default;
+        public string name { get => _Name; set => _Name = value; }
+        public int size { get => _Size; set => _Size = value; }
+        public string type { get => _Type; set => _Type = value; }
+        public object defaultValue { get => _Default; set => _Default = (Float4)value; }
+    }
+}
